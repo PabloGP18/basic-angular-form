57b80c538399a89e1ebe21aafe01c654efff35bc
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-04 16:24:33 +0200

    added the model to the html file

--- Friendsbook/src/app/form-component/form-component.component.ts
+++ Friendsbook/src/app/form-component/form-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Friend} from "../friend";
 
 @Component({
   selector: 'app-form-component',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class FormComponentComponent implements OnInit {
 
   languages = ['html','css','js','php','typescript'];
+
+  friendModel = new Friend('','','','','');
 
   constructor() { }
 

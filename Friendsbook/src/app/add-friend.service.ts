6a20891ff1d204ff50f5baa8fef05bda1940b823
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-05 17:24:41 +0200

    added a plus to the url in the method addfriend so allfriends gets imported withoud writing it

--- Friendsbook/src/app/add-friend.service.ts
+++ Friendsbook/src/app/add-friend.service.ts
@@ -6,13 +6,13 @@ import {Friend} from "./friend";
   providedIn: 'root'
 })
 export class AddFriendService {
-  private _url: string = " ";
+  private _url: string = "http://localhost:PORT/";
 
   constructor( private http: HttpClient) {}
 
   public addFriend(data: Friend)
   {
-    return this.http.post(this._url,data)
+    return this.http.post(this._url + 'allFriend',data)
   }
 
 }

7b23e3994ae63473556f36728660761bd653ee24
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-08 11:36:08 +0200

    added a getter to the url

--- Friendsbook/src/app/add-friend.service.ts
+++ Friendsbook/src/app/add-friend.service.ts
@@ -8,11 +8,15 @@ import {Friend} from "./friend";
 export class AddFriendService {
   private _url: string = "http://localhost:9088/";
 
-  constructor( private http: HttpClient) {}
+  constructor( private _http: HttpClient) {}
 
   public addFriend(data: Friend)
   {
-    return this.http.post(this._url + 'allFriends',data)
+    return this._http.post(this._url + 'allFriends',data)
+  }
+
+  get url(): string {
+    return this._url;
   }
 
 }
